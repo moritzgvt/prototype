fec312cbe5ed7f7d25df907f8767f185f74c35d6
Julien Bisconti <hello@moritzgut.de>
2021-08-30 19:42:57 +0200

    Update REST source

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   BASE_URL: `${window.location.origin}/`,
-  API_LOCATION: 'http://localhost:4040/',
+  API_LOCATION: 'https://moritzgvt.de/',
   API_TYPE: {
     wp: 'wp-json/wp/v2/',
     custom: 'wp-json/custom/v1/',
